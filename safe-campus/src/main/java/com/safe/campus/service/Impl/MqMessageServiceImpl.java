c9fc5545e370f8fe6836a2a70269fb7149528387
axuplus <axuplus@gmail.com>
2020-12-18 14:27:37 +0800

    增加提交白名单

--- safe-campus/src/main/java/com/safe/campus/service/Impl/MqMessageServiceImpl.java
+++ safe-campus/src/main/java/com/safe/campus/service/Impl/MqMessageServiceImpl.java
@@ -1,5 +1,6 @@
 package com.safe.campus.service.Impl;
 
+import com.safe.campus.model.dto.MqSysDto;
 import com.safe.campus.service.MqMessageService;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,9 @@ public class MqMessageServiceImpl implements MqMessageService {
     private AmqpTemplate amqpTemplate;
 
     @Override
-    public Object sendSynchronizeMessages(String routineKey, Object object) {
-        amqpTemplate.convertAndSend("organ.exchange", routineKey, object);
+    public Object sendSynchronizeMessages(String routineKey, String mqSysDto) {
+        amqpTemplate.convertAndSend("organ.local.exchange", routineKey, mqSysDto);
+        // amqpTemplate.convertAndSend("organ.ztgz.exchange", routineKey, mqSysDto);
         return true;
     }
 
