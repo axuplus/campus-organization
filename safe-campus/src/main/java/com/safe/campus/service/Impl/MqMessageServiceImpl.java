664760e96ed15032e8209fe1676ac23f1e5754bd
axuplus <axuplus@gmail.com>
2020-12-26 18:50:43 +0800

    修改mq

--- safe-campus/src/main/java/com/safe/campus/service/Impl/MqMessageServiceImpl.java
+++ safe-campus/src/main/java/com/safe/campus/service/Impl/MqMessageServiceImpl.java
@@ -1,6 +1,5 @@
 package com.safe.campus.service.Impl;
 
-import com.safe.campus.model.dto.MqSysDto;
 import com.safe.campus.service.MqMessageService;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,13 +8,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class MqMessageServiceImpl implements MqMessageService {
 
+
     @Autowired
     private AmqpTemplate amqpTemplate;
 
     @Override
     public Object sendSynchronizeMessages(String routineKey, String mqSysDto) {
-        amqpTemplate.convertAndSend("organ.local.exchange", routineKey, mqSysDto);
-        // amqpTemplate.convertAndSend("organ.ztgz.exchange", routineKey, mqSysDto);
+        amqpTemplate.convertAndSend("people.exchange", routineKey, mqSysDto);
         return true;
     }
 
