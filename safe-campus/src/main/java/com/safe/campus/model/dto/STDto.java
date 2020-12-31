905539fbe5bcb921c86497c2eded2199e75577f3
axuplus <axuplus@gmail.com>
2020-12-31 13:50:24 +0800

    接口取消字段

--- safe-campus/src/main/java/com/safe/campus/model/dto/STDto.java
+++ safe-campus/src/main/java/com/safe/campus/model/dto/STDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 public class STDto {
 
+    @ApiModelProperty(value = "学校ID")
     private Long schoolId;
     private SInfo sInfo;
     private TInfo tInfo;
@@ -13,12 +14,15 @@ public class STDto {
 
     @Data
     public static class SInfo {
+        @ApiModelProperty(value = "学生ID")
         private Long sId;
+        @ApiModelProperty(value = "学号")
         private String sNumber;
         @ApiModelProperty(value = "年级名称")
         private String className;
         @ApiModelProperty(value = "班级名称")
         private String classInfoName;
+        @ApiModelProperty(value = "学生身份证号码")
         private String sIdNumber;
         @ApiModelProperty(value = "1：住校生 2：同校生")
         private Integer type;
@@ -34,11 +38,17 @@ public class STDto {
 
     @Data
     public static class TInfo {
+        @ApiModelProperty(value = "教师ID")
         private Long tId;
         @ApiModelProperty(value = "职务")
         private String position;
+//        @ApiModelProperty(value = "教师角色")
+//        private String teacherRole;
+        @ApiModelProperty(value = "教师电话")
         private Long phone;
+        @ApiModelProperty(value = "教师身份证")
         private String idNumber;
+        @ApiModelProperty(value = "教职工编号")
         private String tNumber;
         @ApiModelProperty(value = "部门")
         private String sectionName;
